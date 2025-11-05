afc9c82815a6df7b00a72e6e5668d813dc262157
JaeilAn <a090066@naver.com>
2025-11-06 00:35:39 +0900

    Initial Commit: Go!Cagong Back file

---
+++ src/main/java/com/inu/go_cagong/GoCagongApplication.java
@@ -0,0 +1,13 @@
+package com.inu.go_cagong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoCagongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoCagongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inu/go_cagong/GoCagongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inu.go_cagong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoCagongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
