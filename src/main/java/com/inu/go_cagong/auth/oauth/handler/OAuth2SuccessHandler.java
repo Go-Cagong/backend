29a5561de90187fe58cbcb92fab3455a5c7972f2
JaeilAn <a090066@naver.com>
2025-11-27 17:17:50 +0900

    fix: 로그인 엔드포인트 수정

--- src/main/java/com/inu/go_cagong/auth/oauth/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/inu/go_cagong/auth/oauth/handler/OAuth2SuccessHandler.java
@@ -12,8 +12,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Component
 @RequiredArgsConstructor
@@ -38,10 +40,19 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         String refreshToken = jwtProvider.createRefreshToken(user.getId(), user.getEmail());
 
         // 쿠키 저장 (60초 = 테스트, 나중에 바꿔도 됨)
-        CookieUtil.addCookie(response, "access_token", accessToken, 60 * 30);
-        CookieUtil.addCookie(response, "refresh_token", refreshToken, 60 * 60 * 24 * 7);
+        //CookieUtil.addCookie(response, "access_token", accessToken, 60 * 30);
+        //CookieUtil.addCookie(response, "refresh_token", refreshToken, 60 * 60 * 24 * 7);
 
         // 로그인 후 리다이렉트 경로
-        response.sendRedirect("/");
+        //response.sendRedirect("/");
+
+        // 주소를 안전하게 만듭니다 (cagong://login-success?access_token=...)
+        String targetUrl = UriComponentsBuilder.fromUriString("cagong://login-success") // 앱을 깨우는 주문
+                .queryParam("access_token", accessToken) // 토큰을 짐으로 실음
+                .queryParam("refresh_token", refreshToken)
+                .build().encode(StandardCharsets.UTF_8).toUriString();
+
+// 만들어진 주소로 쏘세요! -> 앱이 켜집니다.
+        response.sendRedirect(targetUrl);
     }
 }
